5c099cc34a0216d8f62929988ffb87d471532689
unknown <Administrador@TOR-N5A354H72M.ensino.ulbra.net>
2025-02-26 21:50:42 -0300

    first commit

---
+++ src/Livro.java
@@ -0,0 +1,25 @@
+public class Livro {
+    public String titulo;
+    public String autor;
+    public int numeroPagina;
+    public boolean disponibilidade;
+
+    public Livro(String titulo, String autor, int numeroPagina, boolean disponibilidade){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroPagina = numeroPagina;
+        this.disponibilidade = disponibilidade;
+    }
+
+    public void exibirDetalhes(){
+
+    }
+
+    public void reservar(){
+
+    }
+
+    public void devolver(){
+        
+    }
+}
